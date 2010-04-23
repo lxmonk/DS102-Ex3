3a816ecaaa1d2b1c6b5a1766698add4853981302
Aviad Reich <avi.rei@gmail.com>
2010-04-23 19:11:00 +0300

    task 3 for BinarySearchNode amd BinarySearchTree first implementation

--- Skeleton/BinarySearchTree.java
+++ Skeleton/BinarySearchTree.java
@@ -10,29 +10,20 @@ public class BinarySearchTree extends BinaryTree{
 	 * inserts toAdd to its proper place in the search tree
 	 */
 	public void insert(MyObject toAdd) {
-        BinaryNode nxt = this.root;
-        BinaryNode
-        while (nxt != null) {
-            if (nxt.getData().getKeyData().compareTo(toAdd.getKeyData()) < 0) {
-                nxt = nxt.getRight();
-            } else { // toAdd.getKeyData() > nxt.getData().getKeyData()
-                nxt = nxt.getLeft();
-            }
-            nxt =
-        }
+        root.insert(toAdd);
     }
 
 	/*
 	 * removes the object from the search tree
 	 */
-	public void remove(Comparable object){
-		//TODO
-	}
+	public void remove(Comparable object) {
+        root.remove(object);
+    }
 	/*
 	 * Searches the search tree for a node that ovelaps (defined in the assignment description) with the interval [start, end]
 	 * Returns the first overlaping node it encounters in the tree.
 	 */
-	public MyObject OverlapSearch(Comparable start, Comparable end){ 
-		//TODO
-	}
+	public MyObject OverlapSearch(Comparable start, Comparable end) {
+        root.overlapSearch(start, end);
+    }
 }
